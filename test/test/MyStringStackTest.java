c7648f38cbffdf2d45fd0398d0f4467a8b367354
Qi Ming Ng (NCS) <qiming.ng@ncs.com.sg>
2020-08-25 14:53:31 +0800

    fail the test case

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -31,11 +31,10 @@ public class MyStringStackTest extends TestCase {
 
 	@Test
 	public void testPush() {
-		String o1 = null;  
-		MyStringStack stack = new MyStringStack();
-		stack.push (s1);
-		assertTrue (stack.isEmpty());
-		stack.push (s1);
+		String o1 = null;
+        MyStringStack stack = new MyStringStack();
+        assertTrue (stack.isEmpty());
+        stack.push (s1);
         stack.push (s2);
         assertFalse (stack.isEmpty());
 	}
